4b1941c1c3eee56154aca5348c2a9977e869a561
edwardfrary <edwardfrary@gmail.com>
2021-06-08 02:58:00 -0400

    node business

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+
+
+const generatePageCSS = () => {
+    return `
+    a {
+        text-decoration: none;
+        color: black;
+    }
+    `;
+};
+
+fs.writeFile("./index.html", generatePage(name, github), "utf8", err =>{
+    if (err) throw err;
+
+    console.log('HTML GENERATION COMPLETE');
+
+});
+
+fs.writeFile("./assets/style.css", generatePageCSS(), "utf8", err =>{
+    if (err) throw err;
+
+    console.log('CSS GENERATION COMPLETE');
+
+});
