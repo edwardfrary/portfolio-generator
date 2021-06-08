971ad40d2a555c7f33a8678a20ec9281521e7142
edwardfrary <edwardfrary@gmail.com>
2021-06-08 02:16:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
 
-const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length;i+=1){
+        console.log(profileDataArr[i]);
+    }
 
-console.log(generatePage('edward', 'edwardhub'));
+    console.log('=============================');
+
+    profileDataArr.forEach((profileItem) => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
